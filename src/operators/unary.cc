678ca82d56fc6400ced4e011aef4e49adf9f0c0b
Suting Chen <caoster@icloud.com>
2025-01-09 19:35:54 -0600

    HW4

--- src/operators/unary.cc
+++ src/operators/unary.cc
@@ -48,24 +48,54 @@ CastObj::CastObj(GraphObj *graph, Tensor input, Tensor output, CastType type)
 }
 
 vector<DataType> CastObj::inferDataType(const TensorVec &inputs) const {
-  // =================================== 作业
-  // ===================================
-  // TODO：返回经过 cast 操作后, 输出 tensor 的数目和数据类型
-  // REF_FILE: src/core/operator.cc
-  // REF: https://onnx.ai/onnx/operators/onnx__Cast.html#cast-21
-  // =================================== 作业
-  // ===================================
-  return {};
+  switch (castType) {
+  case CastType::Float2BFloat16:
+    return {DataType::BFloat16};
+
+  case CastType::Float2Float16:
+    return {DataType::Float16};
+
+  case CastType::Int322Float:
+  case CastType::Int162Float:
+  case CastType::Int82Float:
+  case CastType::Uint82Float:
+  case CastType::Int642Float:
+  case CastType::Float162Float:
+  case CastType::BFloat162Float:
+  case CastType::Float2Float:
+    return {DataType::Float32};
+
+  case CastType::Int642Uint32:
+    return {DataType::UInt32};
+
+  case CastType::Float2Int8:
+  case CastType::Int322Int8:
+    return {DataType::Int8};
+
+  case CastType::Float2Int16:
+  case CastType::Int322Int16:
+  case CastType::Int82Int16:
+    return {DataType::Int16};
+
+  case CastType::Float2Int32:
+  case CastType::Int162Int32:
+  case CastType::Int82Int32:
+  case CastType::Uint82Int32:
+  case CastType::Int642Int32:
+    return {DataType::Int32};
+
+  case CastType::Float2Int64:
+  case CastType::Int322Int64:
+  case CastType::Uint82Int64:
+  case CastType::Uint322Int64:
+    return {DataType::Int64};
+  }
+  // Never reach
+  IT_ASSERT(false);
 }
 
 optional<vector<Shape>> CastObj::inferShape(const TensorVec &inputs) {
-  // =================================== 作业
-  // ===================================
-  // TODO：返回经过 cast 操作后的 shape
-  // REF: https://onnx.ai/onnx/operators/onnx__Cast.html#cast-21
-  // =================================== 作业
-  // ===================================
-  return std::nullopt;
+  return {{inputs[0]->getDims()}};
 }
 
 std::string CastObj::toString() const {
@@ -128,8 +158,8 @@ DataType CastObj::getOutputDataType() const {
     return DataType::BFloat16;
   case CastType::Float2Float:
     return DataType::Float32;
-        default:
-            IT_TODO_HALT();
-        }
-    }
+  default:
+    IT_TODO_HALT();
+  }
+}
 }; // namespace infini
