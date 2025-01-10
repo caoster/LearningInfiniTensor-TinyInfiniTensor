b0610d0de6af5b10c0bca6ebb6e0839dfad96bf5
Suting Chen <caoster@icloud.com>
2025-01-09 20:30:18 -0600

    HW5

--- src/operators/concat.cc
+++ src/operators/concat.cc
@@ -4,35 +4,42 @@
 namespace infini {
 ConcatObj::ConcatObj(GraphObj *graph, TensorVec inputs, Tensor output, int _dim)
     : OperatorObj(OpType::Concat, inputs, {output}) {
-    int rank = inputs[0]->getRank();
-    dim = get_real_axis(_dim, rank);
-    IT_ASSERT(checkValid(graph));
+  int rank = inputs[0]->getRank();
+  dim = get_real_axis(_dim, rank);
+  IT_ASSERT(checkValid(graph));
 }
 
 optional<vector<Shape>> ConcatObj::inferShape(const TensorVec &inputs) {
-    Shape dims = inputs[0]->getDims();
-    auto rank = inputs[0]->getRank();
+  Shape dims = inputs[0]->getDims();
+  auto rank = inputs[0]->getRank();
 
-    // =================================== 作业 ===================================
-    // TODO：修改 dims，返回正确的 concat 后的 shape
-    // REF: https://onnx.ai/onnx/operators/onnx__Concat.html#concat-13
-    // =================================== 作业 ===================================
+  dims[dim] = 0;
+  for (const auto &input : inputs) {
+    IT_ASSERT(input->getRank() == rank);
+    for (size_t i = 0; i < rank; i++) {
+      if (i == static_cast<size_t>(dim)) {
+        dims[i] += input->getDims()[i];
+      } else {
+        IT_ASSERT(dims[i] == input->getDims()[i]);
+      }
+    }
+  }
 
-    return {{dims}};
+  return {{dims}};
 }
 
 std::string ConcatObj::toString() const {
-    std::ostringstream os;
-    os << "Concat[" << getGuid() << "]";
-    os << "(";
-    for (auto input : inputs)
-        os << vecToString(input->getDims()) << ",";
-    os << "dim=" << dim << ",";
-    os << "input=";
-    for (auto input : inputs)
-        os << input->getGuid() << ",";
-    os << "output=" << outputs[0]->getGuid() << ")";
-    return os.str();
+  std::ostringstream os;
+  os << "Concat[" << getGuid() << "]";
+  os << "(";
+  for (auto input : inputs)
+    os << vecToString(input->getDims()) << ",";
+  os << "dim=" << dim << ",";
+  os << "input=";
+  for (auto input : inputs)
+    os << input->getGuid() << ",";
+  os << "output=" << outputs[0]->getGuid() << ")";
+  return os.str();
 }
 
 } // namespace infini
